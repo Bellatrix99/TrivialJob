61744549b9272795b4824994eade49db7a07f111
Bellatrix99 <1292230729@qq.com>
2021-05-14 12:45:00 +0800

    docs: add Generator.js

--- JS/Generator/Generator.js
+++ JS/Generator/Generator.js
@@ -1,12 +1,26 @@
-function* fibonacci() {
-    let [prev, curr] = [0, 1];
-    for (; ;) {
-        yield curr;
-        [prev, cur] = [curr, prev + curr]
-    }
+// function* fibonacci() {
+//     let [prev, curr] = [0, 1];
+//     while(true){
+//       yield curr;
+//       [prev, curr] = [curr, prev + curr];
+//     }
+//   }
+
+//   for (let n of fibonacci()) {
+//     if (n > 1000) break;
+//     console.log(n);
+//   }
+
+
+function* g() {
+    yield 1;
+    console.log('throwing an exception');
+    yield 2;
+    yield 3;
 }
 
-for (let n of fibonacci()) {
-    if (n > 1000) break;
-    console.log(n);
-}
+let gg = g();
+console.log(gg.next());
+console.log(gg.next());
+// console.log(gg.next());
+
